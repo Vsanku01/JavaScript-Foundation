80c2183d141b3fdd3ecace676992c00a10e6b0a3
Vishnu Darshan Sanku <ethhack001@gmail.com>
2020-09-05 00:27:47 +0530

    Using typeof() keyword for different datatypes

---
+++ Week2/typeOfData.js
@@ -0,0 +1,23 @@
+
+findDataType = () => {
+    console.log(typeof(1)) 
+    console.log(typeof(1.1))
+    console.log(typeof('1.1'))
+    console.log(typeof(true))
+    console.log(typeof(null))
+    console.log(typeof(undefined))
+    console.log(typeof([]))
+    console.log(typeof({}))
+}
+
+findDataType()
+/*
+number
+number
+string
+boolean
+object
+undefined
+object
+object
+*/
