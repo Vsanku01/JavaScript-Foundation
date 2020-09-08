2574d3111b69d2a0e285c91aee20534819f5777c
Vishnu Darshan Sanku <vishnu@Vishnus-MacBook-Pro.local>
2020-09-08 18:56:15 +0530

    completed JSON Objects Exercise

--- Day4-08-09/JS Objects/prob7-jsonCompare.js
+++ Day4-08-09/JS Objects/prob7-jsonCompare.js
@@ -1,5 +1,12 @@
 var expected = {foo: 5, bar: 6};
-var actual = {foo: 5, bar: 6}
+var actual = {foo: 6, bar: 5}
 function assertObjectsEqual(actual, expected, testName){
- // your code here
+    if (JSON.stringify(expected) === JSON.stringify(actual)){
+        console.log('Passed')
+    } else{
+        console.log(`FAILED ${testName} Expected ${expected}, but got ${actual}}`)
+    }
 }
+
+assertObjectsEqual(actual, expected, 'detects that two objects are equal');
+console.log(expected)
