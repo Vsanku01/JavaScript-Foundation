b33957718e0a984467a2f581dbfae7991c3c2808
Vishnu Darshan Sanku <vishnu@Vishnus-MacBook-Pro.local>
2020-09-10 19:58:29 +0530

    Intial Commit

---
+++ Day6-10-09/03-tvClass.js
@@ -0,0 +1,56 @@
+class TV {
+  constructor(_brand, _channel = 1, _price, _inches, _onOffStatus, _volume = 50) {
+    this.brand = _brand;
+    this.channel = _channel;
+    this.price = _price;
+    this.inches = _inches;
+    this.onOffStatus = _onOffStatus;
+    this.volume = _volume;
+  }
+
+  increaseVolume () {
+      if(this.volume >= 0 && this.volume <= 100){
+          this.volume += 1;
+      }
+      console.log('New Volume',this.volume)
+  }
+
+  setChannel() {
+    if(this.channel >= 0 && this.channel <= 50){
+        this.channel += 1;
+    }
+    console.log('New Channel',this.channel)
+  }
+  resetTV() {
+      this.channel = 1;
+      this.volume = 50;
+  }
+
+  status() {
+      console.log('${this.brand} at ${this.channel}, volume {this.volume}')
+  }
+}
+
+class LED extends TV{
+    constructor(_screenThickness, _energyUsage, _lifeSpan, _refreshRate, _viewingAngle, _backLight, _displayDetails){
+        this.screenThickness = _screenThickness;
+        this.energyUsage = _energyUsage;
+        this.lifeSpan = _lifeSpan;
+        this.refreshRate = _refreshRate;
+        this.viewingAngle = _viewingAngle;
+        this.backlight = _backLight;
+        this.displayDetails = _displayDetails;
+    }
+}
+
+class Plasma extends TV{
+    constructor(_screenThickness, _energyUsage, _lifeSpan, _refreshRate, _viewingAngle, _backLight, _displayDetails){
+        this.screenThickness = _screenThickness;
+        this.energyUsage = _energyUsage;
+        this.lifeSpan = _lifeSpan;
+        this.refreshRate = _refreshRate;
+        this.viewingAngle = _viewingAngle;
+        this.backlight = _backLight;
+        this.displayDetails = _displayDetails;
+    }
+}
---
+++ Day6-10-09/04-banking.js
@@ -0,0 +1,81 @@
+class Bank {
+  constructor(_code, _address) {
+    this.codes = _code;
+    this.address = _address;
+  }
+  manages() {
+    // code
+  }
+  maintains() {
+    // code
+  }
+}
+
+class ATM {
+  constructor(_location, _managedby) {
+    this.location = _location;
+    this.managedby = _managedby;
+  }
+}
+
+class Customer extends ATM {
+  constructor(_name, _address, _dob, _cardNumber, _pin) {
+    this.name = _name;
+    this.address = _address;
+    this.dob = _dob;
+    this.cardNumber = _cardNumber;
+    this.pin = _pin;
+  }
+
+  verifyPassword() {
+    // code
+  }
+}
+
+class ATMTransactions {
+  constructor(_transactionId, _date, _type, _amount, _postBalance) {
+    this.transactionId = _transactionId;
+    this.date = _date;
+    this.type = _type;
+    this.amount = _amount;
+    this.postBalance = _postBalance;
+  }
+
+  modifies() {
+    // code
+  }
+}
+
+class Account {
+  constructor(_number, _balance) {
+    this.number = _number;
+    this.balance = _balance;
+  }
+
+  deposit() {
+    // code
+  }
+  withDraw() {
+    // code
+  }
+  createTransaction() {
+    // code
+  }
+}
+
+class currentAccount extends Account {
+  constructor(_accountNo, _balance) {
+    this.accountNo = _accountNo;
+    this.balance = _balance;
+  }
+  withDraw() {
+    // code
+  }
+}
+
+class SavingAccount extends Account {
+  constructor(_accountNo, _balance) {
+    this.accountNo = _accountNo;
+    this.balance = _balance;
+  }
+}
